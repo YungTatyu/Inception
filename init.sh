51670f598f6e12687d031ff73896a8b1cb0ebd26
tt <tt@localhost.localdomain>
2024-08-12 21:53:43 +0900

    modify: init.sh

--- init.sh
+++ init.sh
@@ -1,15 +1,24 @@
 #! /usr/bin/bash
 
+start_services() {
+  # need this to use rc-service
+  openrc
+  touch /run/openrc/softlevel
+  rc-service nginx start
+  rc-service php-fpm83 start
+  rc-service mariadb start
+}
+
 init_mariadb() {
   mkdir /var/lib/mysql
   chown mysql:mysql /var/lib/mysql
 
-  mkdir /run/mysqld
-  chown mysql:mysql /run/mysqld
+  /etc/init.d/mariadb setup
 }
 
 main() {
   init_mariadb
+  start_services
 
   return 0
 }
