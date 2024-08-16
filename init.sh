ed7469948b731de50afe05e33d0235bac529ff81
tt <tt@localhost.localdomain>
2024-08-16 14:25:02 +0900

    modify: init mariadb

--- init.sh
+++ init.sh
@@ -1,26 +1,19 @@
 #! /usr/bin/bash
 
 start_services() {
-  #rc-service nginx start
-  #rc-service php-fpm83 start
-  #rc-service mariadb start
-  # rc-update add nginx default
-  # rc-update add php-fpm83 default
-  # rc-update add mariadb default
   return 0
 }
 
 init_mariadb() {
-  # mkdir /var/lib/mysql
-  # chown mysql:mysql /var/lib/mysql
-
-  # /etc/init.d/mariadb setup
+  mariadb -e "CREATE DATABASE wordpress;"
+  mariadb -e "CREATE USER 'wp'@'%' IDENTIFIED BY 'wp';"
+  mariadb -e "GRANT ALL PRIVILEGES ON wordpress.* TO 'wp'@'%' IDENTIFIED BY 'wp';"
   return 0
 }
 
 main() {
-  # init_mariadb
-  # start_services
+  init_mariadb
+  start_services
 
   return 0
 }
