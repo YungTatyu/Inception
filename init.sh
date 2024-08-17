54ec1b35eb8b02f731ad0e062d612cf95934a815
tachu <>
2024-08-17 19:55:27 +0900

    fix: do not create database and user if already exist

--- init.sh
+++ init.sh
@@ -8,13 +8,13 @@ start_service() {
   local service=$1
   local timeout=$2
   local start_time=$(date +%s) #time in seconds sicne epoch
- 
+
   service ${service} start || return 1
   while ! service ${service} status; do
     echo "waiting ${service} to start"
     sleep 0.5
     local cur_time=$(date +%s)
-    if (( cur_time - start_time > timeout )); then
+    if ((cur_time - start_time > timeout)); then
       err "${service} failed to start"
       return 1
     fi
@@ -29,17 +29,25 @@ stop_service() {
   return 0
 }
 
-
 init_mariadb() {
-  mariadb -e "CREATE DATABASE wordpress;"
-  mariadb -e "CREATE USER 'wp'@'%' IDENTIFIED BY 'wp';"
-  mariadb -e "GRANT ALL PRIVILEGES ON wordpress.* TO 'wp'@'%';"
-  mariadb -e "FLUSH PRIVILEGES;"
+  mariadb -e "
+    -- データベースが存在しない場合のみ作成
+    CREATE DATABASE IF NOT EXISTS wordpress;
+
+    -- ユーザーが存在しない場合のみ作成
+    CREATE USER IF NOT EXISTS 'wp'@'%' IDENTIFIED BY 'wp';
+    
+    -- ユーザーに権限を付与
+    GRANT ALL PRIVILEGES ON wordpress.* TO 'wp'@'%';
+    
+    -- 変更を有効化
+    FLUSH PRIVILEGES;
+  "
   return 0
 }
 
 main() {
-  start_service nginx 0 
+  start_service nginx 0
   start_service php8.2-fpm 0
   start_service mariadb 5 || return 1
   init_mariadb
