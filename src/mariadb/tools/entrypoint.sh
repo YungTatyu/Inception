ed3a4eba4a711e4cb8c0baab22b4b1ad0af4295a
tt <tt@localhost.localdomain>
2024-09-08 11:34:19 +0900

    add: mariadb container

---
+++ src/mariadb/tools/entrypoint.sh
@@ -0,0 +1,36 @@
+#! /usr/bin/bash
+
+err() {
+  echo "[$(date +'%Y-%m-%dT%H:%M:%S%z')]: $*" >&2
+}
+
+setup_mariadb() {
+  if [ ! -e /var/log/mysql ]; then
+    mkdir -m 2750 /var/log/mysql
+    chown mysql /var/log/mysql
+  fi
+  mariadb -e "
+    -- データベースが存在しない場合のみ作成
+    CREATE DATABASE IF NOT EXISTS ${MARIADB_NAME};
+
+    -- ユーザーが存在しない場合のみ作成
+    CREATE USER IF NOT EXISTS '${MARIADB_USER}'@'%' IDENTIFIED BY '${MARIADB_USER}';
+    
+    -- ユーザーに権限を付与
+    GRANT ALL PRIVILEGES ON ${MARIADB_NAME}.* TO '${MARIADB_USER}'@'%';
+    
+    -- 変更を有効化
+    FLUSH PRIVILEGES;
+  " || { err "failed to setup mariadb"; return 1; }
+  return 0
+}
+
+main() {
+  service mariadb start || { err "failed to start mariadb"; return 1; }
+  setup_mariadb || return 1
+
+  bash
+  return 0
+}
+
+main "$@"
